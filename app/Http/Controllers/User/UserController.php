d562f257729eb29a5b0fc2c4e1bb0d1bee8383bf
Ricardo A Leyva <25676407+ricardoandres9728@users.noreply.github.com>
2020-09-30 13:56:34 -0500

    Update UserController.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -19,7 +19,7 @@ class UserController extends ApiController
      */
     public function index()
     {
-        return $this->collectionResponse(UserResource::collection($this->getModel(new User, ['roles'])));
+        return $this->collectionResponse(UserResource::collection($this->getModel(new User, ['roles', 'profile'])));
     }
 
     /**
