92a4fc9d141189a31dc29a19783e68234f1d1111
Ricardo A Leyva <25676407+ricardoandres9728@users.noreply.github.com>
2020-10-27 13:10:57 -0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,9 +21,6 @@ class CreateUsersTable extends Migration
             $table->string('phone')->nullable();
             $table->string('photo')->nullable();
             $table->string('email')->unique()->nullable();
-            $table->string('institution')->unique()->nullable();
-            $table->string('status')->unique()->nullable();
-            $table->string('city')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken();
