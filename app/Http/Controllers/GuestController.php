6a3f803add63767efa690646f358570853e61700
SebastianAldana95 <brayan95sebas@hotmail.com>
2021-03-27 22:35:20 -0500

    cambio asunto de email de confirmacion y redireccion

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Session;
+
 class GuestController extends Controller
 {
     public function verify($code)
-{
-    echo "Entre aqui";	
-    $user = User::where('confirmation_code', $code)->first();
-    echo $user;
-    if (! $user)
-        return redirect('/');
-    $user->confirmed = true;
-    $user->confirmation_code = null;
-    $user->save();
-    echo '<script type="text/javascript">
-         alert("Tarea Guardada");
-        </script>';
-    return redirect('/')->with('notification', 'Has confirmado correctamente tu correo!');
-}
+    {
+        echo "Entre aqui";
+        $user = User::where('confirmation_code', $code)->first();
+        echo $user;
+        if (! $user)
+            return redirect('/');
+        $user->confirmed = true;
+        $user->confirmation_code = null;
+        $user->save();
+        echo '<script type="text/javascript">
+             alert("Tarea Guardada");
+            </script>';
+        return Session::flash('message', 'Bien hecho, Ya puedes iniciar sesion en la App');
+        // return redirect('/')->with('notification', 'Has confirmado correctamente tu correo!');
+    }
 }
