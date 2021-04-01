29ad829f1b49006ed317591b44d22428b7582e1d
SebastianAldana95 <brayan95sebas@hotmail.com>
2021-04-01 10:22:20 -0500

    prueba email verificacion

--- resources/views/confirmation_code_social.blade.php
+++ resources/views/confirmation_code_social.blade.php
@@ -4,15 +4,8 @@
     <meta charset="utf-8">
 </head>
 <body>
-    <h2>Hola {{ $name }}, gracias por registrarte en <strong> MAGIN </strong> !</h2>
-    <p>Por favor confirma tu correo electrónico.</p>
-    <p>Para ello simplemente debes hacer click en el siguiente enlace:</p>
 
-    <a href="{{ url('api/register/verify/' . $confirmation_code) }}">
-        Clic para confirmar tu email
-    </a>
-
-    <p>Despúes de confirmar el correo, inicia sesion con la contraseña: {{$password}} </p>
+    <p>Despúes de confirmar el correo, inicia sesion con la contraseña: pruebatexto </p>
 
 </body>
 </html>
