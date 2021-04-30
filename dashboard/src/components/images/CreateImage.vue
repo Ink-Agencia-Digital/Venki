c10b97ad41e555df1706a9af2d4e3c8ba690c6c2
SebastianAldana95 <brayan95sebas@hotmail.com>
2021-04-30 16:32:41 -0500

    arreglo servicio imagenes

--- dashboard/src/components/images/CreateImage.vue
+++ dashboard/src/components/images/CreateImage.vue
@@ -108,6 +108,9 @@ export default {
                 autoProcessQueue: false,
                 paramName: "url",
                 maxFiles: 1,
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
             },
             loading: null,
         };
